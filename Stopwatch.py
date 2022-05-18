c652650983a8117cf8126ee73b2b62b0c666cacd
CzarKateMendez <105398700+CzarKateMendez@users.noreply.github.com>
2022-05-18 20:30:27 +0800

    Update Stopwatch.py
    Added labels for stopwatch

--- Stopwatch.py
+++ Stopwatch.py
@@ -5,3 +5,11 @@ root=Tk()
 root.configure(background=("#FFFFFF"))
 root.title('Python Project: Stopwatch')
 root.geometry("980x720+220+0")
+
+# labels for stopwatch
+label1 = Label(root, text="HOURS", fg="black", bg="white", font=("Arial", 10, "bold"))
+label1.place(x=280, y=30, width=50, height=30)
+label2 = Label(root, text="MINUTES", fg="black", bg="white", font=("Arial", 10, "bold"))
+label2.place(x=455, y=30, width=80, height=30)
+label3 = Label(root, text="SECONDS", fg="black", bg="white", font=("Arial", 10, "bold"))
+label3.place(x=647, y=30, width=80, height=30)
